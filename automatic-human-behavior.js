b5a0ae7524d8b937e9a138acfb9af7877fb4e401
Errstein <errstein@gmail.com>
2026-03-05 18:32:18 +0700

    Create automatic-human-behavior.js

---
+++ automatic-human-behavior.js
@@ -0,0 +1,8 @@
+<script type='text/javascript'>
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 500) {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({'event': 'high_engagement'});
+  }
+}, {passive: true});
+</script>
