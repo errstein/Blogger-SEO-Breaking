b4d0c52dc48e6aed38bcd1149d2a0162488c58b2
Errstein <errstein@gmail.com>
2026-03-05 18:30:15 +0700

    Create automatic-content-freshness.js

---
+++ automatic-content-freshness.js
@@ -0,0 +1,6 @@
+<script type='text/javascript'>
+  var d = new Date();
+  d.setHours(d.getHours() - 1);
+  var dynamicDate = d.toISOString();
+  // Masukkan variabel dynamicDate ini ke bagian "dateModified" di JSON-LD
+</script>
