46076cccd0b30afa2684942d1102f11ad31fb90e
Errstein <errstein@gmail.com>
2026-03-05 18:30:51 +0700

    Create bounce-rate-killer.js

---
+++ bounce-rate-killer.js
@@ -0,0 +1,10 @@
+<script type='text/javascript'>
+(function() {
+  var targetUrl = "https://www.errstein.com/search/label/AI%20Tools"; 
+  history.pushState(null, null, location.href);
+  window.onpopstate = function() {
+    alert("Tunggu! Lihat tutorial AI terbaru lainnya di sini.");
+    location.href = targetUrl;
+  };
+})();
+</script>
